733d8fb093899a9fa28d68821eacf5a4c27ce9f2
Aryaman <34205435+absterjr@users.noreply.github.com>
2021-08-08 15:10:54 +0530

    Add files via upload

--- Lib/future/html/__init__.py
+++ Lib/future/html/__init__.py
@@ -1,27 +1,31 @@
-"""
-General functions for HTML manipulation, backported from Py3.
+from __future__ import absolute_import
+from future.utils import PY3
+__future_module__ = True
 
-Note that this uses Python 2.7 code with the corresponding Python 3
-module names and locations.
-"""
-
-from __future__ import unicode_literals
-
-
-_escape_map = {ord('&'): '&amp;', ord('<'): '&lt;', ord('>'): '&gt;'}
-_escape_map_full = {ord('&'): '&amp;', ord('<'): '&lt;', ord('>'): '&gt;',
-                    ord('"'): '&quot;', ord('\''): '&#x27;'}
-
-# NB: this is a candidate for a bytes/string polymorphic interface
-
-def escape(s, quote=True):
+if PY3:
+    from html import *
+else:
+    # cgi.escape isn't good enough for the single Py3.3 html test to pass.
+    # Define it inline here instead. From the Py3.4 stdlib. Note that the
+    # html.escape() function from the Py3.3 stdlib is not suitable for use on
+    # Py2.x.
     """
-    Replace special characters "&", "<" and ">" to HTML-safe sequences.
-    If the optional flag quote is true (the default), the quotation mark
-    characters, both double quote (") and single quote (') characters are also
-    translated.
+    General functions for HTML manipulation.
     """
-    assert not isinstance(s, bytes), 'Pass a unicode string'
-    if quote:
-        return s.translate(_escape_map_full)
-    return s.translate(_escape_map)
+
+    def escape(s, quote=True):
+        """
+        Replace special characters "&", "<" and ">" to HTML-safe sequences.
+        If the optional flag quote is true (the default), the quotation mark
+        characters, both double quote (") and single quote (') characters are also
+        translated.
+        """
+        s = s.replace("&", "&amp;") # Must be done first!
+        s = s.replace("<", "&lt;")
+        s = s.replace(">", "&gt;")
+        if quote:
+            s = s.replace('"', "&quot;")
+            s = s.replace('\'', "&#x27;")
+        return s
+
+    __all__ = ['escape']
