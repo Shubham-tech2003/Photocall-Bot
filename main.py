2577eb9d6ec498c73814c37924cefc338388cc70
absterjr <aryamananand5@gmail.com>
2021-08-08 12:37:40 +0530

    commit 6

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ import tweepy
 
 app = Flask(__name__)
 post_desc = input("Desc: ")
+
+class TwitterAPI:
+    consumer_key = "GgQb87lg23UM4dCBDU47NrLPG"
+    consumer_secret = "jlJmy14zU0VW5xk57JiAt2qQxsW9z6tic7HCVNgm88C3HCdbD6"
+    key = "1418207837277200392-VuJhk3Hrs1HAJYLBMiK4vxrqPKOr6V"
+    secret = "Eu9ITJQ1qMLTz7bgT2MVbq2RFytpGdHHDav1SGMUrcWS8"
