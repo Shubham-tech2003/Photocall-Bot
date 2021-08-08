371106ddb004068826154a1193ddffa9b057c247
Abhinav-Modi <abhinavmodi07@gmail.com>
2021-08-08 12:39:29 +0530

    commit_7

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ class TwitterAPI:
     consumer_secret = "jlJmy14zU0VW5xk57JiAt2qQxsW9z6tic7HCVNgm88C3HCdbD6"
     key = "1418207837277200392-VuJhk3Hrs1HAJYLBMiK4vxrqPKOr6V"
     secret = "Eu9ITJQ1qMLTz7bgT2MVbq2RFytpGdHHDav1SGMUrcWS8"
+
+class InstaDetails:
+    instagram_username = "demoforproject"
+    instagram_password = "Shubham2003"
+
+
+"""
+
