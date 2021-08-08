88247de9a7498596c6d661f36e49eb0d7a974f64
Abhinav Modi <81064777+Abhinav-Modi@users.noreply.github.com>
2021-08-08 16:49:13 +0530

    Add files via upload

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -1,34 +1,139 @@
+"""Pillow (Fork of the Python Imaging Library)
+
+Pillow is the friendly PIL fork by Alex Clark and Contributors.
+    https://github.com/python-pillow/Pillow/
+
+Pillow is forked from PIL 1.1.7.
+
+PIL is the Python Imaging Library by Fredrik Lundh and Contributors.
+Copyright (c) 1999 by Secret Labs AB.
+
+Use PIL.__version__ for this Pillow version.
+
+;-)
 """
-    oauthlib
-    ~~~~~~~~
 
-    A generic, spec-compliant, thorough implementation of the OAuth
-    request-signing logic.
+import sys
+import warnings
 
-    :copyright: (c) 2019 by The OAuthlib Community
-    :license: BSD, see LICENSE for details.
-"""
-import logging
-from logging import NullHandler
+from . import _version
 
-__author__ = 'The OAuthlib Community'
-__version__ = '3.1.0'
+# VERSION was removed in Pillow 6.0.0.
+__version__ = _version.__version__
 
-logging.getLogger('oauthlib').addHandler(NullHandler())
 
-_DEBUG = False
+# PILLOW_VERSION is deprecated and will be removed in a future release.
+# Use __version__ instead.
+def _raise_version_warning():
+    warnings.warn(
+        "PILLOW_VERSION is deprecated and will be removed in a future release. "
+        "Use __version__ instead.",
+        DeprecationWarning,
+        stacklevel=3,
+    )
 
-def set_debug(debug_val):
-	"""Set value of debug flag
-	
-    :param debug_val: Value to set. Must be a bool value.
-	"""
-	global _DEBUG
-	_DEBUG = debug_val
 
-def get_debug():
-	"""Get debug mode value. 
-	
-	:return: `True` if debug mode is on, `False` otherwise
-	"""
-	return _DEBUG	
+if sys.version_info >= (3, 7):
+
+    def __getattr__(name):
+        if name == "PILLOW_VERSION":
+            _raise_version_warning()
+            return __version__
+        raise AttributeError("module '{}' has no attribute '{}'".format(__name__, name))
+
+
+else:
+
+    class _Deprecated_Version(str):
+        def __str__(self):
+            _raise_version_warning()
+            return super().__str__()
+
+        def __getitem__(self, key):
+            _raise_version_warning()
+            return super().__getitem__(key)
+
+        def __eq__(self, other):
+            _raise_version_warning()
+            return super().__eq__(other)
+
+        def __ne__(self, other):
+            _raise_version_warning()
+            return super().__ne__(other)
+
+        def __gt__(self, other):
+            _raise_version_warning()
+            return super().__gt__(other)
+
+        def __lt__(self, other):
+            _raise_version_warning()
+            return super().__lt__(other)
+
+        def __ge__(self, other):
+            _raise_version_warning()
+            return super().__gt__(other)
+
+        def __le__(self, other):
+            _raise_version_warning()
+            return super().__lt__(other)
+
+    PILLOW_VERSION = _Deprecated_Version(__version__)
+
+del _version
+
+
+_plugins = [
+    "BlpImagePlugin",
+    "BmpImagePlugin",
+    "BufrStubImagePlugin",
+    "CurImagePlugin",
+    "DcxImagePlugin",
+    "DdsImagePlugin",
+    "EpsImagePlugin",
+    "FitsStubImagePlugin",
+    "FliImagePlugin",
+    "FpxImagePlugin",
+    "FtexImagePlugin",
+    "GbrImagePlugin",
+    "GifImagePlugin",
+    "GribStubImagePlugin",
+    "Hdf5StubImagePlugin",
+    "IcnsImagePlugin",
+    "IcoImagePlugin",
+    "ImImagePlugin",
+    "ImtImagePlugin",
+    "IptcImagePlugin",
+    "JpegImagePlugin",
+    "Jpeg2KImagePlugin",
+    "McIdasImagePlugin",
+    "MicImagePlugin",
+    "MpegImagePlugin",
+    "MpoImagePlugin",
+    "MspImagePlugin",
+    "PalmImagePlugin",
+    "PcdImagePlugin",
+    "PcxImagePlugin",
+    "PdfImagePlugin",
+    "PixarImagePlugin",
+    "PngImagePlugin",
+    "PpmImagePlugin",
+    "PsdImagePlugin",
+    "SgiImagePlugin",
+    "SpiderImagePlugin",
+    "SunImagePlugin",
+    "TgaImagePlugin",
+    "TiffImagePlugin",
+    "WebPImagePlugin",
+    "WmfImagePlugin",
+    "XbmImagePlugin",
+    "XpmImagePlugin",
+    "XVThumbImagePlugin",
+]
+
+
+class UnidentifiedImageError(OSError):
+    """
+    Raised in :py:meth:`PIL.Image.open` if an image cannot be opened and identified.
+    """
+
+    pass
--- Lib/__main__.py
+++ Lib/__main__.py
@@ -1,4 +1,3 @@
-# See http://cens.ioc.ee/projects/f2py2e/
-from numpy.f2py.f2py2e import main
+from .features import pilinfo
 
-main()
+pilinfo()
